fa60dde6b939729c20da5cd4571f8d0a8dd541a3
Carlos Salinas <salinas.carlos@uabc.edu.mx>
2022-01-17 11:29:52 -0800

    Dagger Field Injection

--- app/src/main/java/com/example/android/dagger_example/MainActivity.kt
+++ app/src/main/java/com/example/android/dagger_example/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.android.dagger_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var smartPhone: SmartPhone
+    @Inject
+    lateinit var smartPhone: SmartPhone
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        DaggerSmartPhoneComponent.create().getSmartPhone().makeCall()
+        DaggerSmartPhoneComponent.create().inject(this)
+        smartPhone.makeCall()
 //        val smartPhone = SmartPhone(Battery(), SIMCard(ServiceProvider()), MemoryCard()) //Construct injection
 //        smartPhone.makeCall()
     }
