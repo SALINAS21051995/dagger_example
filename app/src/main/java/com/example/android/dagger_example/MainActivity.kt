20b2513ae9c56f1e23539c33098265d9b6c7d3cc
Carlos Salinas <salinas.carlos@uabc.edu.mx>
2022-01-17 12:19:06 -0800

    Dagger with singleton

--- app/src/main/java/com/example/android/dagger_example/MainActivity.kt
+++ app/src/main/java/com/example/android/dagger_example/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         (application as SmartPhoneApplication).smartPhoneComponent
             .inject(this)
+        smartPhone.makeCall()
     }
 }
